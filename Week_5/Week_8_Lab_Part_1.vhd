6d2aa9c5933a802cad3e45c647502e7d77bf0183
Justin Gray (23295274) <23295274@student.uwa.edu.au>
2023-09-19 11:03:23 +0800

    instantiated some flip flops

--- Week_5/Week_8_Lab_Part_1.vhd
+++ Week_5/Week_8_Lab_Part_1.vhd
@@ -36,32 +36,78 @@ Architecture Behavioural of Week_8_Lab_Part_1 is
 	
 	
 begin
-
-	process(SW0, SW1, KEY0);
 	
 	begin
 	
 		state_0: flip_flop
-			Port Map(Y, CLK, LEDR0);
+			Port Map(
+						D 		=> Y(0);
+						CLK 	=> CLK;
+						Q 		=>	LEDR0
+						);
 		state_1: flip_flop
-			Port Map(Y, CLK, LEDR0);
+			Port Map(
+						D 		=> Y(1);
+						CLK 	=> CLK;
+						Q 		=>	LEDR1
+						);
 		state_2: flip_flop
-			Port Map(Y, CLK, LEDR0);
+			Port Map(
+						D 		=> Y(2);
+						CLK 	=> CLK;
+						Q 		=>	LEDR2
+						);
 		state_3: flip_flop
-			Port Map(Y, CLK, LEDR0);
+			Port Map(
+						D 		=> Y(3);
+						CLK 	=> CLK;
+						Q 		=>	LEDR3
+						);
 		state_4: flip_flop
-			Port Map(Y, CLK, LEDR0);
+			Port Map(
+						D 		=> Y(4);
+						CLK 	=> CLK;
+						Q 		=>	LEDR4
+						);
 		state_5: flip_flop
-			Port Map(Y, CLK, LEDR0);
+			Port Map(
+						D 		=> Y(5);
+						CLK 	=> CLK;
+						Q 		=>	LEDR5
+						);
 		state_6: flip_flop
-			Port Map(Y, CLK, LEDR0);
+			Port Map(
+						D 		=> Y(6);
+						CLK 	=> CLK;
+						Q 		=>	LEDR6
+						);
 		state_7: flip_flop
-			Port Map(Y, CLK, LEDR0);
+			Port Map(
+						D 		=> Y(7);
+						CLK 	=> CLK;
+						Q 		=>	LEDR7
+						);
 		state_8: flip_flop
-			Port Map(Y, CLK, LEDR0);
+			Port Map(
+						D 		=> Y(8);
+						CLK 	=> CLK;
+						Q 		=>	LEDR8
+						);
 		state_9: flip_flop
-			Port Map(Y, CLK, LEDR0);
+			Port Map(
+						D 		=> Y(9);
+						CLK 	=> CLK;
+						Q 		=>	LEDR9
+						);
+		process(SW0, SW1, KEY0);
+		begin
 			
-		
+			case SW0 is
+				when '0' =>
+					y <= '000000000';
+				
+				
+			
+			
 	end
 end Behavioural;

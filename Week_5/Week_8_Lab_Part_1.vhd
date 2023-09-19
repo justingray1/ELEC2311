aa6c75076375201b0efc09de9665a09e2073456e
Justin Gray (23295274) <23295274@student.uwa.edu.au>
2023-09-19 15:35:10 +0800

    progress during lab 5

--- Week_5/Week_8_Lab_Part_1.vhd
+++ Week_5/Week_8_Lab_Part_1.vhd
@@ -18,138 +18,83 @@ entity Week_8_Lab_Part_1 is
 				LEDR3	:	out	STD_LOGIC;	--flip flop state 3
 				LEDR2	:	out	STD_LOGIC;	--flip flop state 2
 				LEDR1	:	out	STD_LOGIC;	--flip flop state 1
-				LEDR0	:	out	STD_LOGIC;	--flip flop state 0
-				Y		:	out	STD_LOGIC_VECTOR(8 down to 0) --Set flip flop states with this
+				LEDR0	:	out	STD_LOGIC	--flip flop state 0
+--				Y		:	out	STD_LOGIC_VECTOR(8 downto 0) --Set flip flop states with this
+				
 			);
 end entity Week_8_Lab_Part_1;
 
 Architecture Behavioural of Week_8_Lab_Part_1 is
 
-	component flip_flop
-		Port	(
-					D		:	in		STD_LOGIC;
-					CLK	:	in		STD_LOGIC;
-					Q		:	out	STD_LOGIC
-				);
-	end component;
-	
+	signal Y_state : std_logic_vector(8 downto 0);
 	
 begin
-	
-	state_0: flip_flop
-		Port Map(
-					D 		=> Y(0),
-					CLK 	=> CLK,
-					Q 		=>	LEDR0
-					);
-	state_1: flip_flop
-		Port Map(
-					D 		=> Y(1),
-					CLK 	=> CLK,
-					Q 		=>	LEDR1
-					);
-	state_2: flip_flop
-		Port Map(
-					D 		=> Y(2),
-					CLK 	=> CLK,
-					Q 		=>	LEDR2
-					);
-	state_3: flip_flop
-		Port Map(
-					D 		=> Y(3),
-					CLK 	=> CLK,
-					Q 		=>	LEDR3
-					);
-	state_4: flip_flop
-		Port Map(
-					D 		=> Y(4),
-					CLK 	=> CLK,
-					Q 		=>	LEDR4
-					);
-	state_5: flip_flop
-		Port Map(
-					D 		=> Y(5),
-					CLK 	=> CLK,
-					Q 		=>	LEDR5
-					);
-	state_6: flip_flop
-		Port Map(
-					D 		=> Y(6),
-					CLK 	=> CLK,
-					Q 		=>	LEDR6
-					);
-	state_7: flip_flop
-		Port Map(
-					D 		=> Y(7),
-					CLK 	=> CLK,
-					Q 		=>	LEDR7
-					);
-	state_8: flip_flop
-		Port Map(
-					D 		=> Y(8),
-					CLK 	=> CLK,
-					Q 		=>	LEDR8
-					);
-	state_9: flip_flop
-		Port Map(
-					D 		=> Y(9),
-					CLK 	=> CLK,
-					Q 		=>	LEDR9
-					);
-	
+
 	process(SW0, SW1, KEY0)
+	
 	begin
 		
 		if SW0 = '0' then
-			y <= "000000000";
+			Y_state <= "000000001";
 		
 		else 
-			case Y is
-				when '000000001' => 
-					if SW1 = '0' then
-						Y <= "000000010";
-					else
-						Y <= "000100000";
-				when '000000010' => 
-					if SW1 = '0' then
-						Y <= "000000100";
-					else
-						Y <= "000100000";
-				when '000000100' => 
-					if SW1 = '0' then
-						Y <= "000001000";
-					else
-						Y <= '000100000';
-				when '000001000' => 
-					if SW1 = '0' then
-						Y <= '000010000';
-					else
-						Y <= '000100000';
-				when '000010000' => 
-					if SW1 = '0' then
-						Y <= '000010000';
-					else
-						Y <= '000100000';
-				when '000100000' => 
-					if SW1 = '0' then
-						Y <= '000000010';
-					else
-						Y <= '001000000'
-				when '001000000' => 
-					if SW1 = '0' then
-						Y <= '000000010';
-					else
-						Y <= '010000000';
-				when '010000000' => 
-					if SW1 = '0' then
-						Y <= '000000010';
-					else
-						Y <= '100000000';
-				when '100000000' => 
-					if SW1 = '0' then
-						Y <= '000000010';
-					else
-						Y <= '100000000';
+			if SW1 = '0' then
+				case Y_state is
+					when "000000001" => 
+						Y_state <= "000000010";
+					when "000000010" => 
+						Y_state <= "000000100";
+					when "000000100" => 
+						Y_state <= "000001000";
+					--when "000001000" => 
+					when "000000000" =>
+						Y_state <= "000010000";
+					when "000010000" => 
+						Y_state <= "000010000";
+					when "000100000" => 
+						Y_state <= "000000010";
+					when "001000000" => 
+						Y_state <= "000000010";
+					when "010000000" => 
+						Y_state <= "000000010";
+					when "100000000" => 
+						Y_state <= "000000010";
+					when others =>
+						Y_state <= "010101010";
+				end case;
+			else
+				case Y_state is
+					when "000000001" => 
+						Y_state <= "000100000";
+					when "000000010" => 
+						Y_state <= "000100000";
+					when "000000100" => 
+						Y_state <= "000100000";
+					when "000001000" => 
+						Y_state <= "000100000";
+					when "000010000" => 
+						Y_state <= "000100000";
+					when "000100000" => 
+						Y_state <= "001000000";
+					when "001000000" => 
+						Y_state <= "010000000";
+					when "010000000" => 
+						Y_state <= "100000000";
+					when "100000000" => 
+						Y_state <= "100000000";
+					when others =>
+						Y_state <= "010101010";
+				end case;
+			end if;
 		end if;
+		LEDR0 <= Y_state(0);
+		LEDR1 <= Y_state(1);
+		LEDR2 <= Y_state(2);
+		LEDR3 <= Y_state(3);
+		LEDR4 <= Y_state(4);
+		LEDR5 <= Y_state(5);
+		LEDR6 <= Y_state(6);
+		LEDR7 <= Y_state(7);
+		LEDR8 <= Y_state(8);
 	end process;
 end Behavioural;

5637849116b492ae7717ad073fc8e0d121e11d70
Justin Gray (23295274) <23295274@student.uwa.edu.au>
2023-09-19 18:47:26 +0800

    finished part 1

--- Week_5/Part_1.vhd
+++ Week_5/Part_1.vhd
@@ -4,110 +4,91 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 
-entity Week_8_Lab_Part_1 is
+entity Part_1 is
 	Port	(
 				SW0	:	in		STD_LOGIC;	--Active-low synchronus reset
 				SW1	:	in 	STD_LOGIC;	--w input
 				KEY0	:	in 	STD_LOGIC;	--clock input
 				LEDR9	:	out 	STD_LOGIC;	--z output
-				LEDR8	:	out	STD_LOGIC:	--flip flop state 8
-				LEDR7	:	out	STD_LOGIC:	--flip flop state 7
-				LEDR6	:	out	STD_LOGIC:	--flip flop state 6
-				LEDR5	:	out	STD_LOGIC:	--flip flop state 5
-				LEDR4	:	out	STD_LOGIC:	--flip flop state 4
-				LEDR3	:	out	STD_LOGIC:	--flip flop state 3
-				LEDR2	:	out	STD_LOGIC:	--flip flop state 2
-				LEDR1	:	out	STD_LOGIC:	--flip flop state 1
-				LEDR0	:	out	STD_LOGIC:	--flip flop state 0
-				Y		:	out	STDLOGIC_VECTOR: (8 down to 0) --Set flip flop states with this
+				LEDR8	:	out	STD_LOGIC;	--flip flop state 8
+				LEDR7	:	out	STD_LOGIC;	--flip flop state 7
+				LEDR6	:	out	STD_LOGIC;	--flip flop state 6
+				LEDR5	:	out	STD_LOGIC;	--flip flop state 5
+				LEDR4	:	out	STD_LOGIC;	--flip flop state 4
+				LEDR3	:	out	STD_LOGIC;	--flip flop state 3
+				LEDR2	:	out	STD_LOGIC;	--flip flop state 2
+				LEDR1	:	out	STD_LOGIC;	--flip flop state 1
+				LEDR0	:	out	STD_LOGIC	--flip flop state 0
 			);
-end week_8_Lab_Pat_1;
+end Part_1;
 
-Architecture Behavioural of Week_8_Lab_Part_1 is
+Architecture Behavioural of Part_1 is
+
+	signal Y				:	STD_LOGIC_VECTOR(8 downto 0);
+	signal Q_signal	:	STD_LOGIC_VECTOR(8 downto 0);
+	signal counter		:	integer := 0;
+	signal CLK			:	STD_LOGIC;
+	
 
 	component flip_flop
 		Port	(
 					D		:	in		STD_LOGIC;
 					CLK	:	in		STD_LOGIC;
 					Q		:	out	STD_LOGIC
-					
 				);
 	end component;
 	
 	
 begin
+	Y0: flip_flop
+		Port Map(Y(0),CLK,Q_signal(0));
+	Y1: flip_flop
+		Port Map(Y(1),CLK,Q_signal(1));
+	Y2: flip_flop
+		Port Map(Y(2),CLK,Q_signal(2));
+	Y3: flip_flop
+		Port Map(Y(3),CLK,Q_signal(3));
+	Y4: flip_flop
+		Port Map(Y(4),CLK,Q_signal(4));
+	Y5: flip_flop
+		Port Map(Y(5),CLK,Q_signal(5));
+	Y6: flip_flop
+		Port Map(Y(6),CLK,Q_signal(6));
+	Y7: flip_flop
+		Port Map(Y(7),CLK,Q_signal(7));
+	Y8: flip_flop
+		Port Map(Y(8),CLK,Q_signal(8));
+					
+	process(SW0, SW1, KEY0)
 	
 	begin
 	
-		state_0: flip_flop
-			Port Map(
-						D 		=> Y(0);
-						CLK 	=> CLK;
-						Q 		=>	LEDR0
-						);
-		state_1: flip_flop
-			Port Map(
-						D 		=> Y(1);
-						CLK 	=> CLK;
-						Q 		=>	LEDR1
-						);
-		state_2: flip_flop
-			Port Map(
-						D 		=> Y(2);
-						CLK 	=> CLK;
-						Q 		=>	LEDR2
-						);
-		state_3: flip_flop
-			Port Map(
-						D 		=> Y(3);
-						CLK 	=> CLK;
-						Q 		=>	LEDR3
-						);
-		state_4: flip_flop
-			Port Map(
-						D 		=> Y(4);
-						CLK 	=> CLK;
-						Q 		=>	LEDR4
-						);
-		state_5: flip_flop
-			Port Map(
-						D 		=> Y(5);
-						CLK 	=> CLK;
-						Q 		=>	LEDR5
-						);
-		state_6: flip_flop
-			Port Map(
-						D 		=> Y(6);
-						CLK 	=> CLK;
-						Q 		=>	LEDR6
-						);
-		state_7: flip_flop
-			Port Map(
-						D 		=> Y(7);
-						CLK 	=> CLK;
-						Q 		=>	LEDR7
-						);
-		state_8: flip_flop
-			Port Map(
-						D 		=> Y(8);
-						CLK 	=> CLK;
-						Q 		=>	LEDR8
-						);
-		state_9: flip_flop
-			Port Map(
-						D 		=> Y(9);
-						CLK 	=> CLK;
-						Q 		=>	LEDR9
-						);
-		process(SW0, SW1, KEY0);
-		begin
-			
-			case SW0 is
-				when '0' =>
-					y <= '000000000';
-				
-				
-			
-			
-	end
+		if SW0 = '0' then --reset is pressed
+			y <= "000000000";
+
+		else
+			if falling_edge(KEY0) then
+				Y(0) <= '1';
+				Y(1) <= (NOT SW1 AND Y(0)) OR (NOT SW1 AND Y(5)) OR (NOT SW1 AND Y(6)) OR (NOT SW1 AND Y(7)) OR (NOT SW1 AND Y(8));
+				Y(2) <= (NOT SW1) AND Y(1);
+				Y(3) <= (NOT SW1) AND Y(2);
+				Y(4) <= ((NOT SW1) AND Y(3)) OR (Y(4) AND (NOT SW1));
+				Y(5) <= (SW1 AND Y(0)) OR (SW1 AND Y(1)) OR (SW1 AND Y(2)) OR (SW1 AND Y(3)) OR (SW1 AND Y(4));
+				Y(6) <= (SW1 AND Y(5));
+				Y(7) <= (SW1 AND Y(6));
+				Y(8) <= (SW1 AND Y(7)) OR (Y(8) AND SW1);
+			end if;
+		end if;
+		
+		LEDR0 <= Y(0);
+		LEDR1 <= Y(1);
+		LEDR2 <= Y(2);
+		LEDR3 <= Y(3);
+		LEDR4 <= Y(4);
+		LEDR5 <= Y(5);
+		LEDR6 <= Y(6);
+		LEDR7 <= Y(7);
+		LEDR8 <= Y(8);
+
+	end process;
 end Behavioural;
---
+++ Week_5/output_files/Part_1.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 19.1.0 Build 670 09/22/2019 SJ Standard Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(10M50DAF484) Path("C:/Users/23295274/Desktop/ELEC2311/Week_5/output_files/") File("Part_1.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
